5a828f9c0b21724fb6672dfccbcce3d1d90dc077
Anna Filatova <filatovaa@yahoo.com>
2020-11-12 18:32:54 -0800

    nav done working on home page

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,3 +1,21 @@
 $(function(){
-  
+    $('.text-link').click(function() {
+        // this removes the underline class from all other ".navigation" links.
+        $('.text-link').removeClass('currentlyActive');
+    
+        // this makes the one that was clicked underlined
+        $(this).addClass('currentlyActive');
+      });
+    
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
