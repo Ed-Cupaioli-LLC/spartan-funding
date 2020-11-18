1810b47ae07050d00b066a2ee00855d2005b7c34
Anna Filatova <filatovaa@yahoo.com>
2020-11-17 17:01:28 -0800

    layout homepage footer and nav completed for tablet and mobile views

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,12 +1,40 @@
 $(function(){
-    $('.text-link').click(function() {
+  $(document).ready(function() {
+    switchCSS($(this).width());
+    console.log('hello')
+    
+    $(window).resize(function() {
+      switchCSS($(this).width());
+    });
+  });
+    $('#nav .text-link').click(function() {
         // this removes the underline class from all other ".navigation" links.
         $('.text-link').removeClass('currentlyActive');
     
         // this makes the one that was clicked underlined
         $(this).addClass('currentlyActive');
       });
+
+
       
+
+      function switchCSS(windowsize) {
+        console.log(windowsize)
+        if (windowsize < 1025) {
+            $('#nav .menu').attr("style", "display: none")
+            $('#nav .hamburger-menu').attr("style", "display: flex")
+
+        } 
+        else{
+          $('#nav .menu').attr("style", "display: flex")
+          $('#nav .hamburger-menu').attr("style", "display: none")
+        }
+      }
+  
+      $('#nav .hamburger-menu').click(function(){
+        
+      })
+    
 
 });
 
