ba3385f6edcadebbfe582d8c1930548d822a6919
Anna Filatova <filatovaa@yahoo.com>
2020-11-20 22:27:04 -0800

    buttons coming form js

--- js/site_functions.js
+++ js/site_functions.js
@@ -79,7 +79,16 @@ $(function(){
     
       
 });
-    
+$("select option").unwrap().each(function() {
+  var btn = $('<div class="btn">'+$(this).text()+'</div>');
+  if($(this).is(':checked')) btn.addClass('on');
+  $(this).replaceWith(btn);
+});
+
+$(document).on('click', '.btn', function() {
+  $('.btn').removeClass('on');
+  $(this).addClass('on');
+});
 
 });
 
