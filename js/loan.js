2208ac50b2d5dfe260a5b397ed890212e24f84f8
Anna Filatova <filatovaa@yahoo.com>
2020-12-04 17:43:33 -0800

    updates to loan page

--- js/loan.js
+++ js/loan.js
@@ -1,57 +1,33 @@
 //Loan Options acordion logic
 $(function() {
-$("#loan-options .option").each(function(){
-       
-    // $(this).children("div:gt(1)").hide();
-    // $(this).children("div").slice(2).hide();
-    // if( $(window).width()> 1000){
-    //   $(this).children('div:eq(1)').attr('style', 'height: calc(3.5rem*3); overflow: hidden;')
-    // }
-    // if( $(window).width()<= 1000){
-    //   $(this).children('div:eq(1)').attr('style', 'height: calc(3.5rem*6); overflow: hidden;')
-    // }
-    // if( $(window).width()<= 500){
-    //   $(this).children('div:eq(1)').attr('style', 'height: calc(3.5rem*7); overflow: hidden; ')
-    // }
-   
-    
-  })
-
-  $("#loan-options .expand").on('click', (function(){
-    // $(this).siblings("div:gt(1)").slideToggle()
-  //   $(this).siblings("div").slice(2).slideToggle()
-  //   if($(this).hasClass('0')){
-  //     $(this).siblings('div:eq(1)').attr('style', 'height: unset; overflow: unset')
-  //     $(this).removeClass('0').addClass('1')
-  //   }
-  //   else {
-  //     if( $(window).width()>1000 && $(this).hasClass('1')){
-  //       $(this).siblings('div:eq(1)').attr('style', 'height: calc(3.5rem*3); overflow: hidden;')
-  //       $(this).removeClass('1').addClass('0')
-  //   }if( $(window).width()<= 1000 && $(this).hasClass('1')){
-  //     $(this).siblings('div:eq(1)').attr('style', 'height: calc(3.5rem*6); overflow: hidden;')
-  //     $(this).removeClass('1').addClass('0')
-  //   }if( $(window).width()<= 500 && $(this).hasClass('1')){
-  //     $(this).siblings('div:eq(1)').attr('style', 'height: calc(3.5rem*9); overflow: hidden;')
-  //     $(this).removeClass('1').addClass('0')
-  //   } 
-  // }
-  //   $(this).siblings("div").slice(2).slideToggle()
-  //   // $(this).slideToggle()
-  // if($(this).hasClass('0')){
-  //   $(this).siblings('div:eq(1)').attr('style', 'height: unset; overflow: unset')
-  //   $(this).removeClass('0').addClass('1')
-  // } else {
-  //   $(this).siblings('div:eq(1)').attr('style', 'max-height: calc(3.5rem*3); overflow: hidden; text-overflow: ellipsis;')
-  //   $(this).removeClass('1').addClass('0')
-  // }
-  // console.log($(this).children('img').attr())
-  console.log($(this).text())
-  $(this).children('span').text($(this).children('span').text() === "Read more" ? "Read less" : "Read more");
-  var src = $(this).children('img').attr('src') === '/img/options/more.png' ? '/img/options/less.png' : '/img/options/more.png';
-  $(this).children('img').attr('src', src)
-  console.log($(this).children('img').attr('src'))
   
-}));
-
-});
+  $("#loan-options .option").each(function(){
+    var content = $(this).children('.content').html();
+    var arr = content.split('<br>');
+    var body = $("<p>").addClass('fun')
+    body.append(arr[0])
+      $(this).children("div:gt(0)").hide();
+      
+     $(this).children('div').children('.sub-title').append(body)
+    })
+  
+    $("#loan-options .expand").on('click', (function(){
+      $(this).siblings('div').find('.fun').hide()
+      $(this).siblings("div:gt(0)").slideToggle()
+      if($(this).hasClass('0')){
+        $(this).siblings('div').find('.fun').hide()
+        $(this).addClass('1').removeClass('0')
+      } else{
+        $(this).siblings('div').find('.fun').show()
+        $(this).addClass('0').removeClass('1')
+      }
+    console.log($(this).text())
+    $(this).children('span').text($(this).children('span').text() === "Read more" ? "Read less" : "Read more");
+    var src = $(this).children('img').attr('src') === '/img/options/more.png' ? '/img/options/less.png' : '/img/options/more.png';
+    $(this).children('img').attr('src', src)
+    console.log($(this).children('img').attr('src'))
+    
+  }));
+  
+  });
+  
