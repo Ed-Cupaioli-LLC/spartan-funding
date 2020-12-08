3464cc27cc0b1ced1ca9e6584118e5cedd99dc1b
Anna Filatova <filatovaa@yahoo.com>
2020-12-08 12:31:05 -0800

    revert

--- js/loan.js
+++ js/loan.js
@@ -5,24 +5,20 @@ $(function() {
     var content = $(this).children('.content').html();
     var arr = content.split('<br>');
     var body = $("<p>").addClass('fun')
-      $(this).children("div:gt(1)").hide();
+    body.append(arr[0])
+      $(this).children("div:gt(0)").hide();
+      
      $(this).children('div').children('.sub-title').append(body)
     })
   
     $("#loan-options .expand").on('click', (function(){
-      var items = $(this).siblings('div').find('.fun')
-      
-      var myArr =  $("#loan-options .option").children('.content').html();
-      console.log('this is items' + myArr)
-      var arr = myArr.split('<br>').shift();
-      console.log(arr)
-      items.append(arr)
-      $(this).siblings("div:gt(1)").slideToggle()
+      $(this).siblings('div').find('.fun').hide()
+      $(this).siblings("div:gt(0)").slideToggle()
       if($(this).hasClass('0')){
-        // $(this).siblings('div').find('.fun').hide()
+        $(this).siblings('div').find('.fun').hide()
         $(this).addClass('1').removeClass('0')
       } else{
-        // $(this).siblings('div').find('.fun').show()
+        $(this).siblings('div').find('.fun').show()
         $(this).addClass('0').removeClass('1')
       }
     console.log($(this).text())
